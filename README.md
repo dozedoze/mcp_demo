3c7dc70a3c0f4dceb5ef119a19e4b2d671927db1
elliot <chenjiaye@bybest.cn>
2025-09-25 11:10:16 +0800

    feat: 提交

--- README.md
+++
@@ -1,2 +0,0 @@
-# mcp_demo
-mcp_demo, 用于学习mcp
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { spawn } = require('child_process');
 
+// 孕育子进程
 const child = spawn('node', ['server.js']);
 
+// 向子进程发送数据
 child.stdin.write('Hello, server!');
 
+// 监听子进程的输出
 child.stdout.on('data', (data) => {
   console.log(`sever: ${data}`);
 });
 
+// 监听子进程的错误输出
 child.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+  console.error(`sever: ${data}`);
 });
