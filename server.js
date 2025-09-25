0b6c330fd555341bf718e62f2c4593a778fd3e4d
elliot <chenjiaye@bybest.cn>
2025-09-25 10:07:47 +0800

    feat: demo1

---
+++ server.js
@@ -0,0 +1,11 @@
+import process from 'process';
+
+
+// step 1 
+process.stdout.write('welcome to mcp demo \n');
+
+
+// step 2
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Received: ${data}`);
+});
